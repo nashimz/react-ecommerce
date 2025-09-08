679b70dfe9c297c4077dd7e611f8755beba3d7a9
Mike <miguelshakur97@gmail.com>
2025-09-08 17:17:25 -0300

    Putting all the changes into my app comp

--- Majors Shop/src/App.jsx
+++ Majors Shop/src/App.jsx
@@ -2,18 +2,34 @@ import "./App.css";
 import { useProducts } from "./hooks/useProducts.js";
 import Products from "./components/Products.jsx";
 import { Navbar } from "./components/Navbar.jsx";
-import { useSearch } from "./hooks/useSearch.js"; // import the hook here
+import { useSearch } from "./hooks/useSearch.js";
+import { useState } from "react";
+import AsideFilters from "./components/AsideFilters.jsx";
 
 function App() {
   const { products: mappedProducts } = useProducts();
   const { search, updateSearch, error } = useSearch();
+  const [filters, setFilters] = useState({
+    category: "all",
+    minPrice: 0,
+    maxPrice: Infinity,
+  });
 
   return (
     <div className="page bg-black/10 min-h-screen">
       <Navbar search={search} updateSearch={updateSearch} error={error} />
-      <main>
-        <div className="flex flex-col items-center ">
-          <Products products={mappedProducts} search={search} />
+      <main className="flex">
+        <AsideFilters
+          filters={filters}
+          setFilters={setFilters}
+          products={mappedProducts}
+        />
+        <div className="flex-1 flex justify-center">
+          <Products
+            products={mappedProducts}
+            search={search}
+            filters={filters}
+          />
         </div>
       </main>
     </div>
