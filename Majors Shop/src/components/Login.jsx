43c327dc606eedf1a2bfc3e2302da9e01df821d7
Mike <miguelshakur97@gmail.com>
2025-09-20 18:29:36 -0300

    feat: Creating custom hook for login and refactor Login.jsx

--- Majors Shop/src/components/Login.jsx
+++ Majors Shop/src/components/Login.jsx
@@ -1,47 +1,13 @@
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useLogin } from "../hooks/useLogin";
 
 export function Login() {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [error, setError] = useState(null);
-  const navigate = useNavigate();
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    setError(null);
-
-    try {
-      const res = await fetch("https://dummyjson.com/user/login", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({
-          username,
-          password,
-          expiresInMins: 30,
-        }),
-      });
-
-      if (!res.ok) {
-        throw new Error("Credenciales inválidas");
-      }
-
-      const data = await res.json();
-
-      // Guardar el token en el almacenamiento local
-      localStorage.setItem("auth", JSON.stringify(data));
-
-      // Redirigir al usuario a la página principal
-      navigate("/");
-    } catch (err) {
-      setError(err.message);
-    }
-  };
+  const { username, setUsername, password, setPassword, error, handleLogin } =
+    useLogin();
 
   return (
     <div className="flex items-center justify-center h-full p-4">
       <form
-        onSubmit={handleSubmit}
+        onSubmit={handleLogin}
         className="bg-white p-6 rounded-xl shadow-md w-80 flex flex-col gap-4"
       >
         <h2 className="text-xl font-bold text-center">Log in</h2>
