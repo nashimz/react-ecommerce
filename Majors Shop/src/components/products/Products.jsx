f6993f1c3adcfda0bce8e39a0ce9cdd920550c0e
Mike <miguelshakur97@gmail.com>
2025-10-08 12:29:22 -0300

    Removing unnecesary styles

--- Majors Shop/src/components/products/Products.jsx
+++ Majors Shop/src/components/products/Products.jsx
@@ -29,7 +29,7 @@ export default function Products({
   const hasResults = filteredProducts.length > 0;
 
   return (
-    <main className="flex flex-col md:flex-row w-full gap-4">
+    <main>
       {hasSearched && hasResults && (
         <AsideFilters
           filters={filters}
