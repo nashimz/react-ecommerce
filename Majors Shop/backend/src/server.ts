c881a54cf95d6f77a2dbc5ded1fb54ba9aa0d4d0
Mike <miguelshakur97@gmail.com>
2025-11-28 17:06:54 -0300

    Fix: cors error

--- Majors Shop/backend/src/server.ts
+++ Majors Shop/backend/src/server.ts
@@ -24,7 +24,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "http://react-ecommerce-sigma-five.vercel.app/",
+      "http://react-ecommerce-sigma-five.vercel.app",
     ],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
